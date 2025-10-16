f24f3eb989ab583f6f3c150b24684327841fe1c0
Rania Zahir <rania.zahir@student.kdg.be>
2025-10-16 15:54:04 +0200

    skeleton project

---
+++ src/test/java/be/kdg/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package be.kdg.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+//TODO: Werk de tests uit vanuit een USer story standpunt niet van methode standpunt.
+	@Test
+	void contextLoads() {
+	}
+
+}
