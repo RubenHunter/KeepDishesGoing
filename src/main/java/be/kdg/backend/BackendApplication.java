2a65db4a3d1d89397436338eb713a788b3bef4c7
ruben-kdg <ruben.moons.1@student.kdg.be>
2026-07-24 00:56:03 +0200

    forgot to add gradle settings

--- src/main/java/be/kdg/backend/BackendApplication.java
+++ src/main/java/be/kdg/backend/BackendApplication.java
@@ -1,15 +1,18 @@
 package be.kdg.backend;
 
+import be.kdg.backend.infrastructure.persistence.payout.PayoutProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableConfigurationProperties(PayoutProperties.class)
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
+
